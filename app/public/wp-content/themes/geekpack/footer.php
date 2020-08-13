8d7f7c38ec669e4861a6d86466aa3c24727d59ad
maddstyle <michaela.decamara@gmail.com>
2020-08-13 01:46:24 -0400

    restructure page layout and width

--- app/public/wp-content/themes/geekpack/footer.php
+++ app/public/wp-content/themes/geekpack/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,26 +12,31 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'geekpack' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'geekpack' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
+</div><!-- #content -->
+</div>
+</div>
+<div class="footer-section full">
+	<div class="wrap">
+		<footer id="colophon" class="site-footer inner">
+			<div class="site-info">
+				<a href="<?php echo esc_url(__('https://wordpress.org/', 'geekpack')); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf(esc_html__('Proudly powered by %s', 'geekpack'), 'WordPress');
+					?>
+				</a>
+				<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'geekpack' ), 'geekpack', '<a href="https://www.geekpack.co">WP Rockstar</a>' );
+				printf(esc_html__('Theme: %1$s by %2$s.', 'geekpack'), 'geekpack', '<a href="https://www.geekpack.co">WP Rockstar</a>');
 				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div>
+	<div>
 
-<?php wp_footer(); ?>
+		<?php wp_footer(); ?>
 
-</body>
-</html>
+		</body>
+
+		</html>
